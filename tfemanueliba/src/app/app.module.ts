905ba7407774ec0815439a23ac3f126c0885780a
Emanuel Ibañez <info.tecno.timon@gmail.com>
2023-03-11 10:36:23 -0300

    modifico seccion proyectos, creo seccion footer

--- tfemanueliba/src/app/app.module.ts
+++ tfemanueliba/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HyssComponent } from './components/hyss/hyss.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { HyssComponent } from './components/hyss/hyss.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HyssComponent
+    HyssComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
