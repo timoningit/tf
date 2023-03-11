6092b3241c4ffd7cf9ae597df45e44715dbc23f4
Emanuel Ibañez <info.tecno.timon@gmail.com>
2023-03-11 02:23:15 -0300

    cambios preproyectos, incluidas hyss

---
+++ tfemanueliba/src/app/components/hyss/hyss.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hyss',
+  templateUrl: './hyss.component.html',
+  styleUrls: ['./hyss.component.css']
+})
+export class HyssComponent {
+
+}
