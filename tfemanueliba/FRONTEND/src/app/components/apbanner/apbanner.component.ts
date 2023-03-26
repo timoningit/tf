62faeacc19c73c11278fe043cb32ad3bbbf52fe1
Emanuel Ibañez <info.tecno.timon@gmail.com>
2023-03-26 01:40:10 -0300

    añado el login y su ruteo al portfolio

--- tfemanueliba/FRONTEND/src/app/components/apbanner/apbanner.component.ts
+++ tfemanueliba/FRONTEND/src/app/components/apbanner/apbanner.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-apbanner',
   templateUrl: './apbanner.component.html',
   styleUrls: ['./apbanner.component.css']
 })
-export class ApbannerComponent {
+export class ApbannerComponent implements OnInit {
+  constructor(private router:Router){ }
 
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
